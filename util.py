6210c75fa49dda10c037de205b6f963c39128824
Adam Amster <adam.amster@alleninstitute.org>
2021-05-26 23:36:41 -0400

    add test

--- util.py
+++ util.py
@@ -5,14 +5,17 @@ import pandas as pd
 import numpy as np
 
 
-def split_image_metadata(path: str, valid_frac):
+def split_image_metadata(path: str, train_frac):
     image_metadata = pd.read_csv(path)
     indices = list(range(image_metadata.shape[0]))
     np.random.shuffle(indices)
-    split = int(np.floor(valid_frac * len(indices)))
-    train_idx, valid_idx = indices[split:], indices[:split]
+    split = int(np.floor(train_frac * len(indices)))
+    midpoint = int((len(indices) - split) / 2)
+    train_idx, valid_idx, test_idx = \
+        indices[:split], indices[split:midpoint], indices[midpoint:]
 
     train = image_metadata.iloc[train_idx]
     valid = image_metadata.iloc[valid_idx]
+    test = image_metadata.iloc[test_idx]
 
-    return train, valid
+    return train, valid, test
